090be08d379a4d91c273da25f41812494e5158b4
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-01-30 18:27:23 +0100

    normalize indention

--- app.js
+++ app.js
@@ -15,33 +15,33 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 function launchIntoFullscreen(element) {
-  if(element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if(element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if(element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if(element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  }
+	if(element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if(element.mozRequestFullScreen) {
+		element.mozRequestFullScreen();
+	} else if(element.webkitRequestFullscreen) {
+		element.webkitRequestFullscreen();
+	} else if(element.msRequestFullscreen) {
+		element.msRequestFullscreen();
+	}
 }
 function exitFullscreen() {
-  if(document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if(document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if(document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  }
+	if(document.exitFullscreen) {
+		document.exitFullscreen();
+	} else if(document.mozCancelFullScreen) {
+		document.mozCancelFullScreen();
+	} else if(document.webkitExitFullscreen) {
+		document.webkitExitFullscreen();
+	}
 }
 
 /*
-Cool pictures to use as background later:
-=========================================
-https://unsplash.com/photos/ooJi3CJQRa8
-https://unsplash.com/photos/PdnseHuDFZU
-https://unsplash.com/photos/2p1HOcpi14U
-https://unsplash.com/photos/UklXbPE-Hos
-https://unsplash.com/photos/U5Ga5VAzdVs
-https://unsplash.com/photos/sgMKvgikHT8
-*/
+ * Cool pictures to use as background later:
+ * =========================================
+ * https://unsplash.com/photos/ooJi3CJQRa8
+ * https://unsplash.com/photos/PdnseHuDFZU
+ * https://unsplash.com/photos/2p1HOcpi14U
+ * https://unsplash.com/photos/UklXbPE-Hos
+ * https://unsplash.com/photos/U5Ga5VAzdVs
+ * https://unsplash.com/photos/sgMKvgikHT8
+ */
